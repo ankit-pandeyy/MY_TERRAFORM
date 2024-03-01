0e7b98188487b0c766db2eec92da4aea1d60b9bc
Himanshu Pandey <60029929+ankit-pandeyy@users.noreply.github.com>
2024-03-01 23:55:26 +0530

    Update Vpc.tf

--- Vpc.tf
+++ Vpc.tf
@@ -117,3 +117,54 @@ resource "aws_instance" "web" {
     Name = "Terraform-01"
   }
 }
+###LB CREATE####
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
+resource "aws_lb_target_group" "front" {
+  name     = "application-front"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 10
+    matcher             = 200
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 3
+    unhealthy_threshold = 2
+  }
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group_attachment
+resource "aws_lb_target_group_attachment" "attach-app1" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.front.arn
+  target_id        = element(aws_instance.web.*.id, count.index)
+  port             = 80
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front.arn
+  }
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
+resource "aws_lb" "front" {
+  name               = "front"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.My_TERRA_VPC_Security_Group.id}"]
+  subnets            = [for subnet in aws_subnet.public_subnets: subnet.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "front"
+  }
+}
